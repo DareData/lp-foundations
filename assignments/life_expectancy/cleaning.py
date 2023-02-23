b778369bc245d5e852a524eb263eee055fe4ec53
Inês Margarida Caetano Correia <32440369+inesmargarida@users.noreply.github.com>
2023-02-23 14:02:19 +0000

    Data cleaning + Refactoring

--- assignments/life_expectancy/cleaning.py
+++ assignments/life_expectancy/cleaning.py
@@ -1,30 +1,47 @@
-import pandas
+"""Module providing function clean_data"""
 import pathlib
+import argparse
+import pandas
 
 PROJECT_DIR = pathlib.Path(__file__).parent
-DATA_DIR = PROJECT_DIR / data
-  
-def clean_data():
-    df = pandas.read_csv(DATA_DIR / 'eu_life_expectancy_raw.tsv'), sep= r'[\t,]', engine = 'python', index_col=False)
+DATA_DIR = PROJECT_DIR / 'data'
 
-    df = df.melt(id_vars = df.columns['unit', 'sex', 'age', ], var_name = 'year', value_name = 'value')
+def clean_data(region_filter :str) -> None:
+    """Cleans data referent to life expectancy in 
+    Europe grouped by Country, Age, Sex and Time."""
+    df_separated = pandas.read_csv(DATA_DIR / 'eu_life_expectancy_raw.tsv',
+                         sep= r'[\t,]', engine = 'python', index_col=False)
 
-    df.rename(columns = {'geo\\time':'region'}, inplace = True)
-    
-    df['year'] = df['year'].apply(pandas.to_numeric, errors='coerce')
+    df_unpivot = df_separated.melt(id_vars = df_separated.columns[:4], var_name = 'year'
+                                   , value_name = 'value')
 
-    df['value'] = df['value'].str.extract(r'(\d*\.?\d*)', expand=False)
-    df['value'] = df['value'].apply(pandas.to_numeric, errors='coerce')
+    df_unpivot.rename(columns = {'geo\\time':'region'}, inplace = True)
 
-    df = df.dropna()
+    df_unpivot['year'] = df_unpivot['year'].apply(pandas.to_numeric, errors='coerce')
 
-    df = df.astype({'year':'int'})
-    df = df.astype({'value':'float'})
+    df_unpivot['value'] = df_unpivot['value'].str.extract(r'(\d*\.?\d*)', expand=False)
+    df_unpivot['value'] = df_unpivot['value'].apply(pandas.to_numeric, errors='coerce')
 
-    df = df[df['region'] == 'PT'] 
-    
-    
-    df.to_csv(DATA_DIR / 'pt_life_expectancy.csv', index=False)
+    df_unpivot = df_unpivot.dropna()
+
+    df_unpivot = df_unpivot.astype({'year':'int'})
+    df_unpivot = df_unpivot.astype({'value':'float'})
+
+    df_unpivot = df_unpivot[df_unpivot['region'] == region_filter]
 
 
-clean_data()
+    df_unpivot.to_csv(DATA_DIR / 'pt_life_expectancy.csv', index=False)
+
+def main():
+    """Main Function"""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--region_filter', type=str, required=False,
+                        default='PT', help='Enter region filter, default \'PT\'')
+
+    args = parser.parse_args()
+
+    clean_data(args.region_filter)
+
+if __name__ == "__main__": #pragma: no cover
+    main()
--- assignments/life_expectancy/tests/test_cleaning.py
+++ assignments/life_expectancy/tests/test_cleaning.py
@@ -7,7 +7,7 @@ from . import OUTPUT_DIR
 
 def test_clean_data(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
-    clean_data()
+    clean_data("PT")
     pt_life_expectancy_actual = pd.read_csv(
         OUTPUT_DIR / "pt_life_expectancy.csv"
     )
