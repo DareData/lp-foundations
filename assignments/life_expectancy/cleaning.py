3328777b1728bb38dea44e9135e73b9fae811dff
Inês Margarida Caetano Correia <32440369+inesmargarida@users.noreply.github.com>
2023-02-23 09:13:50 +0000

    New Path

--- assignments/life_expectancy/cleaning.py
+++ assignments/life_expectancy/cleaning.py
@@ -1,18 +1,19 @@
 import pandas
 import pathlib
 
-path = pathlib.Path.cwd()
-    
+PROJECT_DIR = pathlib.Path(__file__).parent
+DATA_DIR = PROJECT_DIR / data
+  
 def clean_data():
-    df = pandas.read_csv(path.joinpath('./life_expectancy/data/eu_life_expectancy_raw.tsv'), sep= r'[\t,]', engine = 'python', index_col=False)
+    df = pandas.read_csv(DATA_DIR / 'eu_life_expectancy_raw.tsv'), sep= r'[\t,]', engine = 'python', index_col=False)
 
-    df = df.melt(id_vars = df.columns[:4], value_vars=df.columns[4:], var_name = 'year', value_name = 'value')
+    df = df.melt(id_vars = df.columns['unit', 'sex', 'age', ], var_name = 'year', value_name = 'value')
 
     df.rename(columns = {'geo\\time':'region'}, inplace = True)
     
     df['year'] = df['year'].apply(pandas.to_numeric, errors='coerce')
 
-    df['value'] = df['value'].str.extract('(\d*\.?\d*)', expand=False)
+    df['value'] = df['value'].str.extract(r'(\d*\.?\d*)', expand=False)
     df['value'] = df['value'].apply(pandas.to_numeric, errors='coerce')
 
     df = df.dropna()
@@ -23,7 +24,7 @@ def clean_data():
     df = df[df['region'] == 'PT'] 
     
     
-    df.to_csv(path.joinpath('./life_expectancy/data/pt_life_expectancy.csv'), index=False)
+    df.to_csv(DATA_DIR / 'pt_life_expectancy.csv', index=False)
 
 
 clean_data()
