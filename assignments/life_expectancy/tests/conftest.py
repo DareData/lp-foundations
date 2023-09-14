0e3300cdf7ee81943040590bcd1f9d4b5daf5482
Fernando Cordeiro <fernando_cordeiro@live.com>
2023-09-14 16:53:22 +0100

    👌 IMPROVE: Linebreaks

--- assignments/life_expectancy/tests/conftest.py
+++ assignments/life_expectancy/tests/conftest.py
@@ -8,13 +8,17 @@ from . import FIXTURES_DIR, OUTPUT_DIR
 @pytest.fixture(autouse=True)
 def run_before_and_after_tests() -> None:
     """Fixture to execute commands before and after a test is run.
+
     1. Everything you may write before 'yield' will run before the tests
     2. The command 'yield' marks where tests will happen
     3. The code after 'yield' will be run after the tests are finished.
-    Thus, before you refactor the clean_data() function into 3 smaller functions,
-    tests will produce a csv file on the output directory. Since this is bad
-    practise, we are erasing this file to avoid polluting your workspace.
-    After refactoring your functions this code will do nothing."""
+
+    Before you start using your own fixtures, all tests will produce a csv file
+    on the output directory. Since this is bad practise, we are erasing this
+    file to avoid polluting your workspace.
+
+    After refactoring your functions this code will do nothing.
+    """
     # Setup: fill with any logic you want
 
     yield # this is where the testing happens
